50349e27af090757413080f15fd4ee86185bc797
Antonio Aquice <aquice.valentin@gmail.com>
2026-05-29 20:17:41 -0500

    Añadir comentario: Se añadio comentarios para dar mejor explicación del código

--- grados.py
+++ grados.py
@@ -1,6 +1,10 @@
+#Solicitar al usuario que ingrese la temperatura en grados Celsius
 cel = float(input("Ingrese la temperatura en grados Celsius: "))
+
+#Conversión de Celsius a Fahrenheit y Kelvin
 fahr = (cel * 9/5) + 32
 kel = cel + 273.15
 
+#Mostrar los resultados
 print(f"La temperatura en grados Fahrenheit es: {fahr:.2f}")
 print(f"La temperatura en grados Kelvin es:  {kel:.2f}")
