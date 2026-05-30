964ceb0c86f29a0f682b9208fb161aefaca91482
Antonio Aquice <aquice.valentin@gmail.com>
2026-05-29 20:06:15 -0500

    añadir grados.py: convertir grados celsius a fahrenheit y kelvin

---
+++ grados.py
@@ -0,0 +1,6 @@
+cel = float(input("Ingrese la temperatura en grados Celsius: "))
+fahr = (cel * 9/5) + 32
+kel = cel + 273.15
+
+print(f"La temperatura en grados Fahrenheit es: {fahr:.2f}")
+print(f"La temperatura en grados Kelvin es:  {kel:.2f}")
